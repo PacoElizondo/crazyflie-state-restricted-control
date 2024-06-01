30bd69bc5249ca0507c179331bf1663cf774c881
Paco Elizondo <pelizondo@connectia-tech.com>
2024-06-01 15:44:53 -0600

    tuned control

--- my_controller.c
+++ my_controller.c
@@ -37,7 +37,7 @@
 #include "task.h"
 
 #include "controller.h"
-#include "controller_pid.h"
+
 
 #define DEBUG_MODULE "MYCONTROLLER"
 #include "debug.h"
@@ -60,6 +60,8 @@ void appMain() {
 #include "math3d.h"
 #include "physicalConstants.h"
 #include "platform_defaults.h"
+#include "commander.h"
+#include "stabilizer_types.h"
 
 // CONSTANTS
 
@@ -75,20 +77,23 @@ static const float ROTATION_MAX = 30;
 
 
 // const gains
-static const float TRANS_KP_FIXED[] = {4.0f,4.0f,4.0f};
+static const float TRANS_KP_FIXED[] = {7.0f,7.0f,7.0f};
 static const float TRANS_KD_FIXED[] = {3.75f,3.75f,3.75f};
-static const float ROT_KP_FIXED[] = {80.0f,80.0f,80.0f};
-static const float ROT_KD_FIXED[] = {30.0f,30.0f,30.0f};
+static const float ROT_KP_FIXED[] = {90.0f,90.0f,90.0f};
+static const float ROT_KD_FIXED[] = {40.0f,40.0f,40.0f};
 
 // Dynamic gains
-float trans_kp[] = {4.0f,4.0f,4.0f};
+float trans_kp[] = {7.0f,7.0f,7.0f};
 float trans_kd[] = {3.75f,3.75f,3.75f};
-float rot_kp[] = {80.0f,80.0f,80.0f};
-float rot_kd[] = {30.0f,30.0f,30.0f};
+float rot_kp[] = {90.0f,90.0f,90.0f};
+float rot_kd[] = {40.0f,40.0f,40.0f};
 
 
 // Adaptive gains
-static const float LAMBDA[] = {4.0f, 0.5f, 1.5f, 0.5f};
+// static const float LAMBDA[] = {8.0f, 1.0f, 1.5f, 0.05f};
+// static const float ALPHA[] = {7.0f, 0.2f, 8.0f, 0.25f};
+
+static const float LAMBDA[] = {9.0f, 0.75f, 1.5f, 0.5f};
 static const float ALPHA[] = {3.0f, 0.05f, 1.0f, 0.05f};
 
 // Restrictions
@@ -104,8 +109,14 @@ static float orientation_error_stored[] = {0.0f, 0.0f, 0.0f, 0.0f};
 // static float angular_velocity_stored[] = {0.0f,0.0f,0.0f};
 static float angular_velocity_error_stored[] = {0.0f, 0.0f, 0.0f};
 
-//Struct for logging
-static bool isInit = false;
+// Input variables
+static float control_thrust;
+static struct vec control_torque;
+
+
+
+
+static bool isInit;
 
 // Auxiliary functions
 //signum
@@ -196,9 +207,6 @@ void controllerOutOfTree(control_t *control,
   omega[1] = radians(sensors->gyro.y);
   omega[2] = radians(sensors->gyro.z);
 
-  static float control_thrust;
-  static struct vec control_torque;
-
   if (RATE_DO_EXECUTE(UPDATE_RATE, stabilizerStep)) {
 
     // Current attitude
@@ -279,26 +287,13 @@ void controllerOutOfTree(control_t *control,
     orientationDes = qexp(orientationDes);
     orientationDes = qnormalize(orientationDes);
 
-
-
-
-    // orientationDes = qeye();
-
-    // control_thrust = 0.5f*posError.z;
-
     // Orientation error
-    // struct quat orientationErrorPrev = load_q_from_array(orientation_error_stored);
     struct quat orientationError = qqmul(orientation, qinv(orientationDes));
     orientationError = qnormalize(orientationError);
     store_from_q(orientationError, orientation_error_stored);
 
     // Angular Velocity Error
     struct vec orientationErrorVector = rotvec(orientationError); // Euler angle representation
-    // struct vec angVelocityErrorVector = vdiv(rotvec(qqmul(orientationError,qinv(orientationErrorPrev))),DELTA_T);
-    // angular_velocity_error_stored[0] = angVelocityErrorVector.x;
-    // angular_velocity_error_stored[1] = angVelocityErrorVector.y;
-    // angular_velocity_error_stored[2] = angVelocityErrorVector.z;
-
 
     //Angular velocity error from setpoint
     struct vec angVelocityErrorVector = mkvec(
@@ -325,14 +320,10 @@ void controllerOutOfTree(control_t *control,
     struct vec rot_kp_vec = mkvec(rot_kp[0],rot_kp[1],rot_kp[2]);
     struct vec rot_kd_vec = mkvec(rot_kd[0],rot_kd[1],rot_kd[2]);
 
-    // orientation_control = (-ang_kp.*orientation_error_vector)-(ang_kd.*angular_velocity_error);
-    // torque = INERTIA*(orientation_control + cross(angular_velocity,INERTIA*angular_velocity));
     struct vec orientation_control = vsub(veltmul(vneg(rot_kp_vec),orientationErrorVector),veltmul(rot_kd_vec,angVelocityErrorVector));
     // struct vec control_torque = mvmul(CRAZYFLIE_INERTIA,vadd(orientation_control,vcross(angVelocityVector,mvmul(CRAZYFLIE_INERTIA,angVelocityVector))));
     control_torque = mvmul(CRAZYFLIE_INERTIA,orientation_control);
 
-    // Max and min control
-    // 
   }
     // Control Input
   if (setpoint->mode.z == modeDisable) {
@@ -348,7 +339,7 @@ void controllerOutOfTree(control_t *control,
   control->torqueZ  = control_torque.z;
   }  
 
-  // todo: reset variables
+  
 
   control->controlMode = controlModeForceTorque;
 
@@ -359,3 +350,24 @@ void controllerOutOfTree(control_t *control,
 bool controllerOutOfTreeTest(){
   return true;
 }
+
+
+
+LOG_GROUP_START(adaptive_control)
+/**
+ * @brief Thrust
+ */
+LOG_ADD(LOG_FLOAT, thrust, &control_thrust)
+/**
+ * @brief Torque x
+ */
+LOG_ADD(LOG_FLOAT, torque_x, &control_torque.x)
+/**
+ * @brief Torque y
+ */
+LOG_ADD(LOG_FLOAT, torque_y, &control_torque.y)
+/**
+ * @brief Torque z
+ */
+LOG_ADD(LOG_FLOAT, torque_z, &control_torque.z)
+LOG_GROUP_STOP(adaptive_control)
